950518eac8a306b3941f6680cb9a71fe0917ed8f
LydiaThamy <lydiathamy@gmail.com>
2023-04-24 11:37:07 +0800

    closed the scanner

--- App.java
+++ App.java
@@ -7,6 +7,31 @@ public class App
 {
     public static void main( String[] args )
     {
+        // scan methods keeps the console open
+        Scanner scan = new Scanner(System.in);
+
+        // this will exit the app once it prints 'hi there' -- still need to keep the scanner method in a while  loop
+        while (!scan.nextLine().equals("quit")) {
+            System.out.println("hi there");
+        }
+            
+        System.out.println("Are you sure?");
+        
+        while (!scan.nextLine().equals("yes")) {
+            System.out.println("thanks for staying");
+        }
+
+        scan.close();
+        System.out.println("bye bye!");
+
+        /*
+        if (scan.nextLine().equals("yes")) {
+                scan.close();
+                System.out.println("bye bye!");
+        } else if (scan.nextLine().equals("no")) {
+            System.out.println("Thanks for staying");
+        } // scanner will exit after 1 more iteration as it's not kept in a while loop
+
         /*
         // scan methods keeps the console open
         Scanner scan = new Scanner(System.in);
@@ -14,9 +39,12 @@ public class App
         while (!scan.nextLine().equals("quit")) {
             System.out.println("hi there");
         }
+        
+        scan.close();
 
         System.out.println("bye bye!");
         
+        /*
         // test if console methods keep the console open
         Console con = System.console();
         
