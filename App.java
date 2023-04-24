b60bf6ea73782ea9f85ba9c74708e4aa3e466b1f
LydiaThamy <lydiathamy@gmail.com>
2023-04-24 09:39:49 +0800

    scanner and console class

--- App.java
+++ App.java
@@ -1,9 +1,44 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        /*
+        // scan methods keeps the console open
+        Scanner scan = new Scanner(System.in);
+
+        while (!scan.nextLine().equals("quit")) {
+            System.out.println("hi there");
+        }
+
+        System.out.println("bye bye!");
         
+        // test if console methods keep the console open
+        Console con = System.console();
+        
+        String input = "";
+
+        // if I do this, then the system will keep printing
+        while (!input.equals("quit")) {
+            System.out.println("yo bro");
+        }
+        
+        // this works perfectly
+        while (!input.equals("quit")) {
+            input = con.readLine();
+            System.out.println("yo bro");
+        }
+        
+        // this works perfectly
+        while (!con.readLine().equals("quit")) {
+            System.out.println("heyyy");
+        }
+        
+        System.out.println("bye de bye");
+        */
     }
 }
